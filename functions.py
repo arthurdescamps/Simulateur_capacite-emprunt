2a5d91cfa8e582dcbb3b63c11a515c1def82d38a
arthurdescamps <adescamps03.perso@gmail.com>
2023-02-11 11:18:42 +0100

    calcul function

---
+++ functions.py
@@ -0,0 +1,8 @@
+def mensualite_max(revenu) :
+    return 0.35 * revenu
+
+def capacite_emprunt(mensualite_max, taux_credit, taux_assurance, nombre_mensualite) :
+    taux_eq_credit = (1 + taux_credit) ** (1/12) - 1
+    taux_eq_assurance = taux_assurance / 12
+    return mensualite_max / ((taux_eq_credit / (1 - (1 + taux_eq_credit) ** (-nombre_mensualite))) + taux_eq_assurance)
+
