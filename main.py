a5fb5dd55de361daf8cdd78f9209cc3d13f87b93
arthurdescamps <adescamps03.perso@gmail.com>
2023-02-11 11:52:34 +0100

    fixing bug and format

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ revenus = st.number_input("Entrez vos revenus mensuels net", value = 2500)
 mensualite_max = functions.mensualite_max(revenus)
 
 duree_emprunt = st.number_input("Entrez la durée de l'emprunt (en années)", step = 1, value = 20)
-nombre_mensualite = duree_emprunt / 12
+nombre_mensualite = duree_emprunt * 12
 
-taux_credit = st.number_input("Entrez le taux du crédit", step = 0.0001, value = 0.02)
-taux_assurance = st.number_input("Entrez le taux d'assurance", step = 0.0001, value = 0.008)
+taux_credit = st.number_input("Entrez le taux du crédit", step = 0.0001, value = 0.02, format = "%0.4f")
+taux_assurance = st.number_input("Entrez le taux d'assurance", step = 0.0001, value = 0.008, format = "%0.4f")
 
 capacite_emprunt = functions.capacite_emprunt(mensualite_max, taux_credit, taux_assurance, nombre_mensualite)
 st.write("Votre capacité d'emprunt maximale est de :", capacite_emprunt)
