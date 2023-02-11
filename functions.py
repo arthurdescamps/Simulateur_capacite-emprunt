d2caa8cc14b113ba80f8aac768b6978ae2483134
arthurdescamps <adescamps03.perso@gmail.com>
2023-02-11 11:22:54 +0100

    add loyers function

--- functions.py
+++ functions.py
@@ -6,3 +6,6 @@ def capacite_emprunt(mensualite_max, taux_credit, taux_assurance, nombre_mensual
     taux_eq_assurance = taux_assurance / 12
     return mensualite_max / ((taux_eq_credit / (1 - (1 + taux_eq_credit) ** (-nombre_mensualite))) + taux_eq_assurance)
 
+def prise_en_compte_loyer(loyers) :
+    return 0.7 * loyers
+
