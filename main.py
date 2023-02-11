be17765cbd65a1521d7a783de4de7bd3b473565a
arthurdescamps <adescamps03.perso@gmail.com>
2023-02-11 11:35:18 +0100

    fixing bug

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ st.title("Calculatrice de la capacité d'emprunt")
 revenus = st.number_input("Entrez vos revenus mensuels net")
 mensualite_max = functions.mensualite_max(revenus)
 
-duree_emprunt = st.number_input("entrez la durée de l'emprunt (en années)")
+duree_emprunt = st.number_input("Entrez la durée de l'emprunt (en années)")
 nombre_mensualite = duree_emprunt / 12
 
-taux_credit = st.number_input("Entrez le taux du crédit")
-taux_assurance = st.number_input("Entrez le taux d'assurance")
+taux_credit = st.number_input("Entrez le taux du crédit", step = 0.0001)
+taux_assurance = st.number_input("Entrez le taux d'assurance", step = 0.0001)
 
 capacite_emprunt = functions.capacite_emprunt(mensualite_max, taux_credit, taux_assurance, nombre_mensualite)
 st.write("Votre capacité d'emprunt maximale est de :", capacite_emprunt)
